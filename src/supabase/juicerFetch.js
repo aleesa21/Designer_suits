7930beb6cb44fb3232b775d81e66804e27e88dff
yudin101 <contact@yudin101.com.np>
2026-08-31 15:39:13 +0545

    feat: add filter in fetchSocialsFeed function

--- src/supabase/juicerFetch.js
+++ src/supabase/juicerFetch.js
@@ -1,14 +1,35 @@
 import { supabase } from "./commonFunctions";
 
 // Fetch socials feed
-export async function fetchSocialsFeed() {
+// Args:
+//  - site: "instagram" || "tiktok" || "all"
+//    - The function will filter the posts from the provided platform
+//    - "all" returns everything
+export async function fetchSocialsFeed(site) {
   try {
     const { data } = supabase.storage
       .from("social-feed")
       .getPublicUrl("juicer-fetched-data.json");
 
     const response = await fetch(data.publicUrl);
-    const feed = await response.json();
+
+    if (!response.ok) {
+      return {
+        success: false,
+        error: `Failed to fetch socials feed: HTTP ${response.status}`,
+      };
+    }
+
+    let feed = await response.json();
+
+    // Filter if site is not "all"
+    if (site !== "all") {
+      const filteredPosts = feed.posts.filter(
+        // Actually source.source
+        (p) => p.source.source.toLowerCase() === site.toLowerCase(),
+      );
+      feed = { ...feed, posts: filteredPosts };
+    }
 
     return {
       success: true,
