5b82e99aead3efd620a38fee1de0ab1314978522
fatpotatosalad <jonathondudu07@gmail.com>
2026-08-31 11:32:33 +0545

    Revert "new CI pipeline + tests"
    This reverts commit 185595739b6ad2198227b0d357a507a366367feb.

--- src/test/setup.js
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom/vitest";
-import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
-
-// Auto-unmount React trees after each test so state doesn't leak between them.
-afterEach(() => {
-  cleanup();
-});
